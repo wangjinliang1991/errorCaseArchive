e69cf7aa382de08e685d12e0b33563590f101762
wangjinliang1991 <wjl31802@126.com>
2023-08-28 09:28:59 +0800

    feature: 数字的处理

--- README.md
+++ README.md
@@ -3,3 +3,50 @@
 
 ## numbers
 
+### 问题1
+org.example.double01.DoubleTest.testDouble01 为何 `0.1+0.2 != 0.3`
+
+计算机是以二进制存储数值的，参考 https://www.zhihu.com/question/21711083
+
+实际 0.1 的二进制表示为 0.0 0011 0011 0011… （0011 无限循环)
+
+因此，0.1 无法精确表达
+
+那如何处理？BigDecimal类型
+
+### 问题2
+
+org.example.double01.DoubleTest.testBigDecimal01 使用了BigDecimal还不行？
+
+BigDecimal 表示和计算浮点数，且务必使用字符串的构造方法来初始化 BigDecimal
+
+参见 org.example.double01.DoubleTest.testBigDecimal02
+
+### 问题3
+
+假设是Double类型，能否用toString然后做BigDecimal处理？
+
+`new BigDecimal(Double.toString(4.015)).multiply(new BigDecimal(Double.toString(100)))`得到结果是`401.5000`
+
+跟我们预期的 `401.5` 不一致，为什么呢？
+
+debug发现 BigDecimal 有 两个属性
+- scale: 小数点右边的位数 如 4.015是3
+- precision: 表示精度 也就是有效数字的长度，是4
+
+而 100的分别是1,4
+对于乘法操作，返回值的 scale 是两个数的 scale 相加，最后结果小数点右边为4，`401.5000`
+
+除了scale的不准确，那精度有没有问题？
+
+### 问题4
+四舍五入的问题如何处理？
+org.example.double01.DoubleTest.testPrecision 结果double的合适，float的 `3.35f` 变成 `3.3`
+
+为啥？
+double 和 float 的 3.35 其实相当于 3.350xxx 和 3.349xxx
+
+那如何控制呢？设置 DecimalFormat 格式
+
+
+
--- src/test/java/org/example/double01/DoubleTest.java
+++ src/test/java/org/example/double01/DoubleTest.java
@@ -2,6 +2,10 @@ package org.example.double01;
 
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+
 public class DoubleTest {
     @Test
     public void testDouble01() {
@@ -19,5 +23,45 @@ public class DoubleTest {
         }
     }
 
+    @Test
+    public void testBigDecimal01() {
+        System.out.println(new BigDecimal(0.1).add(new BigDecimal(0.2)));
+        System.out.println(new BigDecimal(1.0).subtract(new BigDecimal(0.8)));
+        System.out.println(new BigDecimal(4.015).multiply(new BigDecimal(100)));
+        System.out.println(new BigDecimal(123.3).divide(new BigDecimal(100)));
+    }
 
+    @Test
+    public void testBigDecimal02() {
+        System.out.println(new BigDecimal("0.1").add(new BigDecimal("0.2")));
+        System.out.println(new BigDecimal("1.0").subtract(new BigDecimal("0.8")));
+        System.out.println(new BigDecimal("4.015").multiply(new BigDecimal("100")));
+        System.out.println(new BigDecimal("123.3").divide(new BigDecimal("100")));
+    }
+
+    @Test
+    public void testDouble02() {
+        System.out.println(new BigDecimal(Double.toString(4.015)).multiply(new BigDecimal(Double.toString(100))));
+    }
+
+    @Test
+    public void testPrecision() {
+        double num1 = 3.35;
+        float num2 = 3.35f;
+
+        System.out.println(String.format("%.1f", num1));//四舍五入
+        System.out.println(String.format("%.1f", num2));
+    }
+
+    @Test
+    public void testDecimalFormat() {
+        double num1 = 3.35;
+        float num2 = 3.35f;
+        DecimalFormat format = new DecimalFormat("#.##");
+
+        format.setRoundingMode(RoundingMode.DOWN);
+        System.out.println(format.format(num1));
+        format.setRoundingMode(RoundingMode.DOWN);
+        System.out.println(format.format(num2));
+    }
 }
